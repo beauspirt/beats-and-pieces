4942e5ea933fb66fc3adcf6084da32905a7f6eda
Beats & Pieces <contact@beatsandpieces.ro>
2026-08-24 22:50:08 +0300

    Update footer social icons with official links and order

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,12 +14,12 @@ export const Footer: React.FC = () => {
 
         {/* Right: Social Icons */}
         <div className="flex items-center gap-5 text-[#888888]">
-          {/* Discord Icon */}
+          {/* 1. Discord */}
           <a
-            href="https://discord.gg"
+            href="https://www.discord.gg/DJRwTDM7Dr"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-[#7B61FF] transition-colors"
+            className="hover:text-[#5865F2] transition-colors"
             aria-label="Discord Server"
           >
             <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
@@ -27,12 +27,25 @@ export const Footer: React.FC = () => {
             </svg>
           </a>
 
-          {/* YouTube Icon */}
+          {/* 2. Instagram */}
           <a
-            href="https://youtube.com"
+            href="https://www.instagram.com/beatsandpieces.ro"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-red-500 transition-colors"
+            className="hover:text-[#E4405F] transition-colors"
+            aria-label="Instagram Page"
+          >
+            <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
+              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+            </svg>
+          </a>
+
+          {/* 3. YouTube */}
+          <a
+            href="https://www.youtube.com/@beatsandpieces_ro"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-[#FF0000] transition-colors"
             aria-label="YouTube Channel"
           >
             <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
@@ -40,16 +53,42 @@ export const Footer: React.FC = () => {
             </svg>
           </a>
 
-          {/* Instagram Icon */}
+          {/* 4. Bandcamp */}
           <a
-            href="https://instagram.com"
+            href="https://beatsandpiecesro.bandcamp.com"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-pink-500 transition-colors"
-            aria-label="Instagram Page"
+            className="hover:text-[#1DA0C3] transition-colors"
+            aria-label="Bandcamp Page"
           >
             <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
-              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+              <path d="M0 18.75l7.437-13.5H24l-7.438 13.5H0z"/>
+            </svg>
+          </a>
+
+          {/* 5. Facebook */}
+          <a
+            href="https://www.facebook.com/beatsandpieces.ro"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-[#1877F2] transition-colors"
+            aria-label="Facebook Page"
+          >
+            <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
+              <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+            </svg>
+          </a>
+
+          {/* 6. Patreon */}
+          <a
+            href="https://www.patreon.com/beatsandpieces"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-[#FF424D] transition-colors"
+            aria-label="Patreon Page"
+          >
+            <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
+              <path d="M15.386.5C10.74.5 7 4.244 7 8.89c0 4.583 3.74 8.297 8.386 8.297 4.618 0 8.364-3.714 8.364-8.297C23.75 4.244 20.004.5 15.386.5zM0 23.5h4.15V.5H0v23z" />
             </svg>
           </a>
         </div>
